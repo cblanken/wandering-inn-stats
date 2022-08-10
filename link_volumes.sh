62307857a813a1d086a9785b770085f60ce42b9a
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-08-09 22:34:55 -0400

    Update link script for volume 9 up to 'Interlude- Mundanity and Memorials'

--- link_volumes.sh
+++ link_volumes.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Create volume directories
-for i in {1..8}; do
+for i in {1..9}; do
     dir="volume$i"
     if [ ! -d "./volumes/$dir" ]; then
         mkdir -p "./volumes/$dir"
     fi
 done
 
-ln -sr ./chapters/{1..64}-* ./volumes/volume1/
-ln -sr ./chapters/{65..121}-* ./volumes/volume2/
-ln -sr ./chapters/{122..173}-* ./volumes/volume3/
-ln -sr ./chapters/{174..236}-* ./volumes/volume4/
-ln -sr ./chapters/{237..308}-* ./volumes/volume5/
-ln -sr ./chapters/{309..385}-* ./volumes/volume6/
-ln -sr ./chapters/{386..480}-* ./volumes/volume7/
-ln -sr ./chapters/{481..585}-* ./volumes/volume8/
+ln -srf ./chapters/{1..64}-* ./volumes/volume1/
+ln -srf ./chapters/{65..121}-* ./volumes/volume2/
+ln -srf ./chapters/{122..173}-* ./volumes/volume3/
+ln -srf ./chapters/{174..236}-* ./volumes/volume4/
+ln -srf ./chapters/{237..308}-* ./volumes/volume5/
+ln -srf ./chapters/{309..385}-* ./volumes/volume6/
+ln -srf ./chapters/{386..480}-* ./volumes/volume7/
+ln -srf ./chapters/{481..587}-* ./volumes/volume8/
+ln -srf ./chapters/{588..597}-* ./volumes/volume9/
