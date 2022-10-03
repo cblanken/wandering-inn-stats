a606b5e347b5039544f400b80c0af199f1e61f60
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-10-03 19:08:42 -0400

    Refactor get.py to follow PyLint recommendations

--- get.py
+++ get.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python
-
-# Download every chapter from the links in the Wandering Inn Table of Contents
-from sys import argv, exit
+"""Module to download every chapter from the links in the Wandering Inn Table of Contents"""
+from sys import argv
+from os import path, sep, getcwd, mkdir
+from time import sleep
 from requests import get
 from bs4 import BeautifulSoup
-from os import path, sep, getcwd, mkdir 
-from time import sleep
 
 BASE_URL = "https://wanderinginn.com/table-of-contents/"
 BASE_PATH = "chapters"
 if not path.isdir(BASE_PATH):
     mkdir(BASE_PATH)
 
-def getChapterLinks():
+def get_chapter_links():
+    """Scrape table of contents for chapter links
+    """
     table_of_contents = get(BASE_URL)
     if table_of_contents.status_code != 200:
         print(f"Cannot access {BASE_URL}. Check your network connection.")
@@ -21,11 +21,12 @@ def getChapterLinks():
     soup = BeautifulSoup(table_of_contents.content, 'html.parser')
     chapter_link_elements = soup.select(
             '#content div.entry-content > p > a[href^="https://wanderinginn.com"]')
-    chapter_links = list(filter(None, [link.get('href') for link in chapter_link_elements]))
-    return chapter_links
+    return list(filter(None, [link.get('href') for link in chapter_link_elements]))
 
-def getChapterText(link):
-    page = get(link)
+def get_chapter_text(chapter_link):
+    """Scrape chapter text from link
+    """
+    page = get(chapter_link)
     soup = BeautifulSoup(page.content, 'html.parser')
     header_text = [element.get_text() for element in soup.select(
         '#content > article > header.entry-header')]
@@ -33,49 +34,48 @@ def getChapterText(link):
         '#content > article > div.entry-content > *')]
     return "".join(header_text) + "\n\n".join(content_text)
 
-def saveChapter(filename, content):
-    with open(filename, "w") as file:
+def save_chapter(file, content):
+    """Write chapter text content to file
+    """
+    with open(file, "w", encoding="utf-8") as file:
         file.write(content)
 
 if __name__ == "__main__":
-    chapter_links = getChapterLinks()
+    chapter_links = get_chapter_links()
     max_chapter = len(chapter_links)
-    if len(argv) == 1:
-        offset = 0
-    elif len(argv) == 2:
+    OFFSET = 0
+    if len(argv) == 2:
         try:
             # Max offset of 1000
-            offset = min(int(argv[1]), 1000)
+            OFFSET = min(int(argv[1]), 1000)
         except TypeError:
             print("Invalid offset argument. Enter a number between 1 and 1000.")
             exit(0)
     elif len(argv) > 2:
         try:
             # Max offset of 1000
-            offset = min(int(argv[1]), 1000)
+            OFFSET = min(int(argv[1]), 1000)
             max_chapter = min(int(argv[2]), max_chapter)
         except TypeError:
             print("Invalid offset argument. Enter a number between 1 and 1000.")
             exit(0)
-    else:
-        offset = 0
 
     # TODO handle keyboard interrupt
     # TODO add pause/resume
     # TODO add type hinting
     # TODO add chapter hashing to check for changes
     # TODO add chapter archiving functionality
-    for i, link in enumerate(chapter_links[offset:max_chapter]): 
-        file_prefix = f"{offset + i}-"
+    for i, link in enumerate(chapter_links[OFFSET:max_chapter]):
+        file_prefix = f"{OFFSET + i}-"
 
         # remove trailing '/' from URL
-        file_suffix = str(link[8:-1]) if link[-1] == "/" else str(link[8:])
+        FILE_SUFFIX = str(link[8:-1]) if link[-1] == "/" else str(link[8:])
 
-        filename = file_prefix + file_suffix.replace(sep, '-')
-        filepath = sep.join([getcwd(), BASE_PATH, filename])
-        if not path.exists(filepath) and link != None:
-            print("Downloading {} to {}".format(link, filepath))
-            saveChapter(filepath, getChapterText(link))
+        filename = file_prefix + FILE_SUFFIX.replace(sep, '-')
+        FILEPATH = sep.join([getcwd(), BASE_PATH, filename])
+        if not path.exists(FILEPATH) and link is not None:
+            print(f"Downloading {link} to {FILEPATH}")
+            save_chapter(FILEPATH, get_chapter_text(link))
             sleep(0.25)
         else:
-            print("Skipping {}, {} already exists".format(link, filename))
+            print(f"Skipping {link}, {filename} already exists")
