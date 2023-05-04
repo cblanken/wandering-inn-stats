7a9a82fb6dbf738e9ae84e8ed5afb55d3b2064eb
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-05-04 16:09:59 -0400

    Rework processing to be object oriented
    - Move processing models to __init__.py
    - Add Volume, Book, and Chapter objects to model directory structure
    - Add Pattern Enum to apply multiple regex patterns when parsing text for TextRef(s)

--- processing/__init__.py
+++ processing/__init__.py
@@ -1,99 +1,234 @@
 """Module for processing scraped chapter text"""
+from __future__ import annotations
 import re
+import sys
 from collections import OrderedDict
+from enum import Enum, Flag, auto
 from pathlib import Path
-from . models import DEFAULT_CONTEXT_LEN, TextRef
+from typing import Generator
 
-MAGIC_WORD_PATTERN = r"\[(\w\,? ?)+\]"
-ALL_MAGIC_WORDS_RE = re.compile(MAGIC_WORD_PATTERN)
+DEFAULT_CONTEXT_LEN = 50
 
-OBTAINED_PATTERN = r".*[Oo]btained.?\]$"
-SKILL_OBTAINED_RE = re.compile(r"^\[[Ss]kill" + OBTAINED_PATTERN)
-CLASS_OBTAINED_RE = re.compile(r"\[.*[Cc]lass" + OBTAINED_PATTERN)
-SPELL_OBTAINED_RE = re.compile(r"^\[[Ss]pell" + OBTAINED_PATTERN)
-
-def get_text_refs(pattern: re.Pattern, lines: list[str],
-    context_len: int = DEFAULT_CONTEXT_LEN) -> list[TextRef]:
-    """Return list of TextRef(s) for a given regex pattern over [lines] of text
+class Color(Enum):
     """
-    text_refs = []
-    matches_per_line = [re.finditer(pattern, line) for line in lines]
-    for line_id, matches in enumerate(matches_per_line):
-        for match in matches:
-            text_ref = TextRef(match.group(), match.string, line_id, match.start(),
-                               match.end(), context_len)
-            text_refs.append(text_ref)
-    return text_refs
+    Enum to specify colored text in the book
+    """
 
-def generate_chapter_text_refs(
-    filepath: Path,
-    include_classes_obtained: bool = False,
-    include_skills_obtained: bool = False,
-    include_spells_obtained: bool = False):
-    """Return  TextRef(s) that match the regex for the [MAGIC_WORDS] and optionally
-    obtained [Classes], [Skills], or [Spells]
+    # Classes and Skills
+    RED_LEVELING = ["FF0000"] # red skills & classes
+    RED_SER_RAIM = ["EB0E0E"] # Ser Raim
+    RED_FIRE = ["E01D1D"] # Ivolethe fire
+    PINK_CHARM = ["FDDBFF", "FFB8FD", "FD78FF", "FB00FF"] # Magnolia Reinhart charm
+    YELLOW_DIVINE_TEMP = ["FFD700"] # Divine and temp skills
+    GREEN_UNIQUE = ["99CC00"] # Unique skills & classes
+    BLUE_CLASS_RESTORATION = ["#99CCFF"] # Class restoration
+    BLUE_COLD = ["CCFFFF", "99CCFF", "3366FF"] # Cold-based skills
+    BLUE_WATER = ["00CCFF"] # Water-based skills
 
+    # Antinium Colors
+    # TODO: add missing Antinium queen colors
+    YELLOW_GRAND_QUEEN = ["FFCC00"] # Antinium Grand Queen speech
+    GREEN_FLYING_QUEEN = ["99CC00"] # Antinium Flying Queen speech
+    PURPLE_SILENT_QUEEN = ["CC99FF"]
+    GRAY_SILENT_QUEEN = ["999999"]
+    BROWN_TWISTED_QUEEN = ["993300"]
+
+    # Fae
+    GREEN_SPRINT_FAE = ["96BE50"]
+    BLUE_WINTER_FAE = ["8AE8FF"]
+
+    # Hidden text
+    BLACK_INVIS = "0C0E0E"
+
+    NORMAL = "EEEEEE"
+
+OBTAINED_SUFFIX = r".*[Oo]btained.?\]"
+class Pattern(Enum):
+    """Text matching RE patterns for processing chapter text"""
+    ALL_MAGIC_WORDS = re.compile(r"\[(\w\,? ?)+\]")
+    SKILL_OBTAINED = re.compile(r"\[[Ss]kill" + OBTAINED_SUFFIX)
+    CLASS_OBTAINED = re.compile(r"\[.*[Cc]lass" + OBTAINED_SUFFIX)
+    SPELL_OBTAINED = re.compile(r"\[[Ss]pell" + OBTAINED_SUFFIX)
+
+    @classmethod
+    def _or(cls, *patterns: Pattern):
+        if len(patterns) == 1:
+            return patterns[0].value
+
+        new_pattern = re.compile("|".join([p.value.pattern for p in patterns]))
+        return new_pattern
+
+    @classmethod
+    def _and(cls, *patterns: Pattern):
+        # TODO: implement for combining Pattern with AND
+        pass
+
+class RefType(Enum):
+    """Text reference types"""
+    CHARACTER = auto()
+    ITEM = auto()
+    SKILL = auto()
+    CLASS = auto()
+    SPELL = auto()
+    MIRACLE = auto()
+    OBTAINED = auto()
+
+class TextRef:
+    """
+    A Text Reference to a specified keyword in the text
+
+    Properties:
+    - phrase (str): Keyphrase found
+    - line (int): Line number in the text
+    - start_column (int): Column number of first letter in (phrase) found in the text
+    - end_col (int): Column number of last letter in (phrase) found in the text
+    - context (str): Contextual text surrounding (phrase)
+    - type (RefType): Type of refence such as Characer, Class, Spell etc.
+    """
+    def __init__(self, text: str, line_text: str, line_id: int, start_column: int,
+        end_column: int, context_offset: int = DEFAULT_CONTEXT_LEN) -> TextRef:
+        self.text: str = text.strip()
+        self.line_number: int = line_id
+        self.start_column: int = start_column
+        self.end_column: int = end_column
+        self.context_offset: str = context_offset
+        self.type: RefType = None
+
+        # Construct surrounding context string
+        start = max(start_column - context_offset, 0)
+        end = min(end_column + context_offset, len(line_text))
+        self.context = line_text[start:end].strip()
+
+    def __str__(self):
+        return f"Line: {self.line_number:>5}: {self.text:⋅<55}context: {self.context}"
+
+    def classify_text_ref(self):
+        """Interactive classification of TextRef type"""
+        print(self)
+        try:
+            sel = input(
+                "Classify the above TextRef ([ch]aracter, [it]em, [sk]ill, [cl]ass, [sp]ell, "
+                "[mi]racle, [ob]tained, leave blank to skip): "
+            )
+
+            while True:
+                if sel.strip() == "":
+                    print("> TextRef skipped!\n")
+                    return None
+                if len(sel) < 2:
+                    print("Invalid selection.")
+                    yes_no = input("Try again (y/n)")
+                    if yes_no.lower() == "y":
+                        continue
+                    return None
+                break
+
+            match sel[:2].lower():
+                case "ch":
+                    self.type = RefType.CHARACTER
+                case "it":
+                    self.type = RefType.ITEM
+                case "sk":
+                    self.type = RefType.SKILL
+                case "cl":
+                    self.type = RefType.CLASS
+                case "sp":
+                    self.type = RefType.SPELL
+                case "mi":
+                    self.type = RefType.MIRACLE
+                case "ob":
+                    self.type = RefType.OBTAINED
+
+            print(f"> classified as {self.type}\n")
+            return self
+        except KeyboardInterrupt:
+            sys.exit()
+        except EOFError:
+            sys.exit()
+
+class Chapter:
+    """Model for chapter as a file
+    
     Args:
-    - filepath (Path): file system path to chapter text file
+    - title (str): Chapter title
+    - url (str): URL of chapter at wandering.com
+    - path (Path): path to HTML file of downloaded chapter
     """
-    with open(filepath, "r", encoding="utf-8") as file:
-        lines = file.readlines()
+    def __init__(self, title: str, path: Path):
+        self.title: str = title
+        self.path: Path = path
+        self.all_text_refs: Generator = self.gen_chapter_text_refs(Pattern._or(
+            Pattern.ALL_MAGIC_WORDS,
+            Pattern.SKILL_OBTAINED,
+            Pattern.CLASS_OBTAINED,
+            Pattern.SPELL_OBTAINED
+        ))
 
-    refs = get_text_refs(ALL_MAGIC_WORDS_RE, lines)
-    if len(refs) > 0:
-        for ref in refs:
-            yield ref
+    def gen_chapter_text_refs(self, pattern: re.Pattern, context_len: int = DEFAULT_CONTEXT_LEN):
+        """Return  TextRef(s) that match the regex for the given regex patterns
 
-    if include_classes_obtained:
-        class_refs = get_text_refs(CLASS_OBTAINED_RE, lines)
-        if len(class_refs) > 0:
-            refs += class_refs
-            for ref in class_refs:
-                yield ref
+        Args:
+        - patterns (Patterns): Bitwise combination of Patterns
+        """
+        with open(self.path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
 
-    if include_skills_obtained:
-        skill_refs = get_text_refs(SKILL_OBTAINED_RE, lines)
-        if len(skill_refs) > 0:
-            refs += skill_refs
-            for ref in skill_refs:
-                yield ref
+        matches_per_line = [re.finditer(pattern, line) for line in lines]
+        for line_number, matches in enumerate(matches_per_line):
+            for match in matches:
+                yield TextRef(match.group(), match.string, line_number,
+                            match.start(), match.end(), context_len)
 
-    if include_spells_obtained:
-        spell_refs = get_text_refs(SPELL_OBTAINED_RE, lines)
-        if len(spell_refs) > 0:
-            refs += spell_refs
-            for ref in spell_refs:
-                yield ref
-
-def print_chapter_text_refs(chapter_path: Path):
-    """Print references for single chapter at `chapters_path`
-    """
-    print("")
-    print("=" * len(str(chapter_path)))
-    print(chapter_path)
-    print("=" * len(str(chapter_path)))
-    for ref in generate_chapter_text_refs(chapter_path, True, True, True):
-        print(ref)
-
-
-def get_text_ref_generators_by_chapter_title(volumes_dir: Path) -> OrderedDict:
-    """Print references for all chapters
-    """
-    chapter_paths: list[Path] = list(Path(volumes_dir).glob("**/*.txt"))
-    chapter_paths.sort(key=lambda n: f"{''.join([x.split('_')[0] for x in n.parts[1:]]):0>7}")
-
-    gens_by_chapter = OrderedDict()
-    for path in chapter_paths:
-         gens_by_chapter[path] = generate_chapter_text_refs(path, True, True, True)
-    return gens_by_chapter
-
-def print_all_text_refs(volumes_dir: Path):
-    """Print all text references found by `generate_all_text_refs` generator
-    """
-    for title, generator in get_text_ref_generators_by_chapter_title(volumes_dir).items():
+    def print_all_text_refs(self):
+        """Print TextRef(s) for chapter
+        """
+        headline = f"{self.title} - {self.path}"
         print("")
-        print("=" * len(str(title)))
-        print(title)
-        print("=" * len(str(title)))
-        for ref in generator:
+        print("=" * len(headline))
+        print(headline)
+        print("=" * len(headline))
+        for ref in self.all_text_refs:
             print(ref)
+
+    def __str__(self):
+        return f"{self.title}: {self.path}"
+
+
+class Book:
+    """Model for book as a file"""
+    def __init__(self, title: str, path: Path):
+        self.title = title
+        self.path = path
+        self.chapter_paths: list[Path] = list(Path(self.path).glob("*.html"))
+        self.chapter_paths.sort(
+            key=lambda n: f"{''.join([x.split('_')[0] for x in n.parts[1:]]):0>7}")
+
+        self.chapters: OrderedDict[Chapter] = OrderedDict()
+        for chapter_path in self.chapter_paths:
+            title = chapter_path.stem.split('_')[1]
+            self.chapters[title] = Chapter(title, chapter_path)
+
+    def __str__(self):
+        return f"{self.title}: {self.path}"
+
+class Volume:
+    """Model for Volume as a file"""
+    def __init__(self, title: str, path: Path):
+        self.title: str = title
+        self.path: Path = path
+        book_paths: list[Path] = [dir for dir in Path(path).iterdir() if dir.is_dir()]
+        self.books: OrderedDict[Book] = OrderedDict()
+        for book_path in book_paths:
+            assert "Book" in str(book_path)
+            title = book_path.name.split('_')[1]
+            self.books[title] = Book(title, book_path)
+
+    def print_all_text_refs(self):
+        """Print all text references found by `generate_all_text_refs` generator
+        """
+        for book in self.books:
+            for chapter in book.chapters:
+                chapter.print_all_text_refs()
+
+    def __str__(self):
+        return f"{self.title}: {self.path.absolute()}"
--- processing/__main__.py
+++ processing/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-from . import get_text_ref_generators_by_chapter_title, print_all_text_refs
+from . import Volume
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -10,5 +10,13 @@ if __name__ == "__main__":
 
     parser.add_argument("path", type=Path, help="Path to volumes")
     args = parser.parse_args()
-    
-    print_all_text_refs(Path(args.path))
+
+    volume_paths = [x for x in Path(args.path).iterdir() if x.is_dir() and "Volume" in x.name]
+    for p in volume_paths:
+        vol = Volume(p.name.split('_')[1], p)
+        print("")
+        print(vol)
+        for book in vol.books.values():
+            print(book)
+            for chapter in book.chapters.values():
+                chapter.print_all_text_refs()
