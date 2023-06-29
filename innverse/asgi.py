9cc7f9467b660e465ef378a02643addc7c689d85
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-06-29 15:23:27 -0400

    Rename root project from innverse_stats to innverse

--- innverse/asgi.py
+++ innverse/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for innverse_stats project.
+ASGI config for innverse project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innverse_stats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innverse.settings')
 
 application = get_asgi_application()
--- innverse/wsgi.py
+++ innverse/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for innverse_stats project.
+WSGI config for innverse project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innverse_stats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innverse.settings')
 
 application = get_wsgi_application()
