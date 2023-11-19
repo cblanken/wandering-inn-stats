462809282dc0b53333a68c1715e4d616e2cb724b
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-11-19 11:29:15 -0500

    Refactor search form
    - Use built-in Django widget to render select/option inputs
    - Add default First/Last selection for first and laster chapter inputs
    - Add custom SearchForm ChoiceField tailwind classes
    - Fix `type` input not getting passed to rendered search form with query

--- innverse/forms.py
+++ innverse/forms.py
@@ -5,11 +5,22 @@ MAX_CHAPTER_NUM = int(Chapter.objects.all().order_by("-number")[0].number)
 
 
 def get_chapters():
-    return ((c.number, c.title) for c in Chapter.objects.all().order_by("number"))
+    yield (0, "--- First Chapter ---")
+    for tup in ((c.number, c.title) for c in Chapter.objects.all().order_by("number")):
+        yield tup
+    yield (MAX_CHAPTER_NUM + 1, "--- Last Chapter ---")
+
+
+select_input_tailwind_classes = "bg-bg-primary text-text-primary border-none"
 
 
 class SearchForm(forms.Form):
-    type = forms.ChoiceField(label="Type", choices=RefType.TYPES, required=False)
+    type = forms.ChoiceField(
+        label="Type",
+        choices=RefType.TYPES,
+        required=False,
+        widget=forms.Select(attrs={"class": select_input_tailwind_classes}),
+    )
     type_query = forms.CharField(label="Type Query", max_length=50, required=False)
     text_query = forms.CharField(label="Text Query", max_length=100, required=False)
     first_chapter = forms.TypedChoiceField(
@@ -18,11 +29,13 @@ class SearchForm(forms.Form):
         empty_value=8,
         required=False,
         initial=0,
+        widget=forms.Select(attrs={"class": select_input_tailwind_classes}),
     )
     last_chapter = forms.TypedChoiceField(
         label="Last Chapter",
         choices=get_chapters,
         empty_value=MAX_CHAPTER_NUM,
         required=False,
-        initial=MAX_CHAPTER_NUM,
+        initial=MAX_CHAPTER_NUM + 1,
+        widget=forms.Select(attrs={"class": select_input_tailwind_classes}),
     )
