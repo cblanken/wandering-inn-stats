7ba31b103e6b70dc229f11a25e86e6faad652045
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2024-01-22 22:39:18 -0500

    Allow conditional chart thumbnail generation by name

--- stats/management/commands/generate_chart_thumbnails.py
+++ stats/management/commands/generate_chart_thumbnails.py
@@ -5,6 +5,14 @@ from stats import charts
 class Command(BaseCommand):
     help = "Generate all chart thumbnails to static svg files"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--chart-name",
+            type=str,
+            default="",
+            help="Only generate thumbnails for charts which contain the given `--chart-name`",
+        )
+
     def handle(self, *args, **options) -> None:
         chart_galleries = [
             charts.word_count_charts,
@@ -14,12 +22,19 @@ class Command(BaseCommand):
         try:
             for gallery in chart_galleries:
                 for chart in gallery:
-                    chart.save_thumbnail()
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'Chart ({chart.title}) saved to "{chart.path}"'
+                    if options.get("chart_name") in chart.title_slug:
+                        chart.save_thumbnail()
+                        self.stdout.write(
+                            self.style.SUCCESS(
+                                f'Chart ({chart.title}) saved to "{chart.path}"'
+                            )
                         )
-                    )
+                    else:
+                        self.stdout.write(
+                            self.style.WARNING(
+                                f'Chart ({chart.title}) did not match chart-name: "{options.get("chart_name")}"'
+                            )
+                        )
         except KeyboardInterrupt as exc:
             raise CommandError(
                 "Keyboard interrupt...thumbnail generation stopped."
