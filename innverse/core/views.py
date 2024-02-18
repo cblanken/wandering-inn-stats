9f921732c668cbcdb2daaa93445dee199022cea5
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2024-02-18 12:52:32 -0500

    Fix favicon routing to static root

--- innverse/core/views.py
+++ innverse/core/views.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from django.conf import settings
 from django.http import FileResponse, HttpRequest, HttpResponse
 from django.views.decorators.cache import cache_control
@@ -7,5 +8,5 @@ from django.views.decorators.http import require_GET
 @require_GET
 @cache_control(max_age=60 * 60 * 24, immutable=True, public=True)
 def favicon(request: HttpRequest) -> HttpResponse:
-    file = (settings.BASE_DIR / "innverse" / "static" / "favicon.svg").open("rb")
+    file = Path(settings.STATIC_ROOT, "favicon.svg").open("rb")
     return FileResponse(file)
