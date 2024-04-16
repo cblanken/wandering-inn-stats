d7ed32b08cfc68ef5857ed2d4c4eabdaf08ce1bd
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2024-04-16 19:13:30 -0400

    Re-add request parameter for favicon route
    Evidentally, an HttpRequest parameter is required by Django even if it's unused

--- innverse/core/views.py
+++ innverse/core/views.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from django.conf import settings
-from django.http import FileResponse
+from django.http import FileResponse, HttpRequest
 from django.views.decorators.cache import cache_control
 from django.views.decorators.http import require_GET
 
 
 @require_GET
 @cache_control(max_age=60 * 60 * 24, immutable=True, public=True)
-def favicon() -> FileResponse:
+def favicon(request: HttpRequest) -> FileResponse:
     file = Path(settings.STATIC_ROOT, "favicon.svg").open("rb")
     return FileResponse(file)
