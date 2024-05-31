8f57fe3df0583ecdf73deec7c40e64335bf46214
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2024-05-30 22:04:16 -0400

    Fix missing favicon
    Relying on the Django favicon.ico was failing to load the favicon.svg so links for
    favicon.svg and favicon.ico were added to the base template.

--- innverse/urls.py
+++ innverse/urls.py
@@ -3,10 +3,7 @@ from django.apps import apps
 from django.http import HttpResponseRedirect
 from django.urls import path, include, reverse
 
-# from django.shortcuts import redirect
-
 from . import views
-from .core import views as core_views
 
 
 def overview_redirect(req):
@@ -43,7 +40,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("__reload__", include("django_browser_reload.urls")),
-    path("favicon.ico", core_views.favicon),
 ]
 
 
