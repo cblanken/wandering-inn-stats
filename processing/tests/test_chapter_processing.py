fda4dab09f9ce9b6df5cc257ede7233a3d5d8fb4
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-09-08 21:08:28 -0400

    refactor: chapter processing tests
    Rename fixtures (and backing files) and organize into classes to make
    it simpler to organize additional tests.

--- processing/tests/test_chapter_processing.py
+++ processing/tests/test_chapter_processing.py
@@ -4,200 +4,179 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 
 
-@pytest.fixture
-def ch1_html_content() -> BeautifulSoup:
-    """Sample chapter #1"""
-    with Path.open(Path(__file__).parent / "samples/8.00/8.00.html", encoding="utf-8") as fp:
-        soup = BeautifulSoup(fp)
-        soup.get("html")
-        return soup
+class TestChapterProcessing_8_00:
+    @pytest.fixture
+    def html_content(scope="class") -> BeautifulSoup:
+        """Sample chapter #1"""
+        with Path.open(Path(__file__).parent / "samples/8.00/chapter.html", encoding="utf-8") as fp:
+            soup = BeautifulSoup(fp)
+            soup.get("html")
+            return soup
+
+    @pytest.fixture
+    def text_content(scope="class") -> str:
+        """Sample Author's Note #1"""
+        with Path.open(Path(__file__).parent / "samples/8.00/chapter.txt", encoding="utf-8") as fp:
+            return fp.read()
+
+    @pytest.fixture
+    def authors_note(scope="class") -> str:
+        """Sample Author's Note #1"""
+        with Path.open(Path(__file__).parent / "samples/8.00/authors_note.txt", encoding="utf-8") as fp:
+            return fp.read()
+
+    @pytest.fixture
+    def pre_note(scope="class") -> str:
+        """Sample parenthesized note #1"""
+        with Path.open(Path(__file__).parent / "samples/8.00/pre_note.txt", encoding="utf-8") as fp:
+            return fp.read()
+
+    def test_text_content(self, html_content: BeautifulSoup, text_content: str):
+        """Most author's notes appear at the end of the chapter with the typical 'Author's Note' indicator"""
+        data = parse_chapter_content(html_content)
+        assert data.get("text") == text_content
+
+    def test_author_note_at_start(self, pre_note: str):
+        """Some author's notes appear at the start of the chapter with the typical 'Author's Note' indicator"""
+        # TODO
+
+    def test_author_note_at_end(self, html_content: BeautifulSoup, authors_note: str):
+        """Most author's notes appear at the end of the chapter with the typical 'Author's Note' indicator"""
+        data = parse_chapter_content(html_content)
+        assert data.get("authors_note") == authors_note
+
+    def test_parenthesized_author_note_at_start(self, html_content: BeautifulSoup, pre_note: str):
+        """Sometimes pirateaba provides a short aside in parentheses in the first couple lines"""
+        data = parse_chapter_content(html_content)
+        assert data.get("pre_note") == pre_note
 
 
-@pytest.fixture
-def ch1_text_content() -> str:
-    """Sample Author's Note #1"""
-    with Path.open(Path(__file__).parent / "samples/8.00/8.00.txt", encoding="utf-8") as fp:
-        return fp.read()
+class TestChapterProcessing_TheRoots3:
+    @pytest.fixture
+    def html_content(scope="class") -> BeautifulSoup:
+        """Sample chapter #2"""
+        with Path.open(Path(__file__).parent / "samples/TheRoots3/chapter.html", encoding="utf-8") as fp:
+            soup = BeautifulSoup(fp)
+            soup.get("html")
+            return soup
+
+    @pytest.fixture
+    def text_content(scope="class") -> str:
+        """Sample Author's Note #2"""
+        with Path.open(Path(__file__).parent / "samples/TheRoots3/chapter.txt", encoding="utf-8") as fp:
+            return fp.read()
+
+    @pytest.fixture
+    def authors_note(scope="class") -> str:
+        """Sample Author's Note #2"""
+        with Path.open(Path(__file__).parent / "samples/TheRoots3/authors_note.txt", encoding="utf-8") as fp:
+            return fp.read()
+
+    @pytest.fixture
+    def pre_note(scope="class") -> str:
+        """Sample pre note #2"""
+        with Path.open(Path(__file__).parent / "samples/TheRoots3/pre_note.txt", encoding="utf-8") as fp:
+            return fp.read()
+
+    def test_text_does_not_contain_authors_note(self, html_content: BeautifulSoup):
+        """The chapter text should not contain any text from author's note"""
+        data = parse_chapter_content(html_content)
+        authors_note = data.get("authors_note")
+        if authors_note is None:
+            msg = "Empty author's note"
+            raise ValueError(msg)
+        for line in authors_note.split("\n"):
+            if not line.isspace and line != "":
+                assert line not in data.get("text")
+
+    def test_signed_pre_note_detected(self, html_content: BeautifulSoup):
+        """Signed pre-notes by the author should not be detected by parser"""
+        data = parse_chapter_content(html_content)
+        pre_note = data.get("pre_note")
+
+        if pre_note is None:
+            msg = "Empty author's note"
+            raise ValueError(msg)
+
+        assert (
+            pre_note
+            == "Did you click ‘Slam’ on last chapter and find the other two parts? If you didn’t, go and do that.\n—pirateaba\n"
+        )
+
+    def test_signed_pre_note_not_in_chapter_text(self, html_content: BeautifulSoup):
+        """Signed pre-notes by the author should not be included in the chapter text"""
+        data = parse_chapter_content(html_content)
+        pre_note = data.get("pre_note")
+
+        if pre_note is None:
+            msg = "Empty author's note"
+            raise ValueError(msg)
+        for line in pre_note.split("\n"):
+            if not line.isspace and line != "":
+                assert line not in data.get("text")
 
 
-@pytest.fixture
-def ch1_authors_note() -> str:
-    """Sample Author's Note #1"""
-    with Path.open(Path(__file__).parent / "samples/8.00/8.00_authors_note.txt", encoding="utf-8") as fp:
-        return fp.read()
+class TestChapterProcessing_10_22_R:
+    @pytest.fixture
+    def html_content(scope="class") -> BeautifulSoup:
+        """Sample chapter #3"""
+        with Path.open(Path(__file__).parent / "samples/10.22_R/chapter.html", encoding="utf-8") as fp:
+            soup = BeautifulSoup(fp)
+            soup.get("html")
+            return soup
 
+    @pytest.fixture
+    def text_content(scope="class") -> str:
+        """Sample chapter #3"""
+        with Path.open(Path(__file__).parent / "samples/10.22_R/chapter.txt", encoding="utf-8") as fp:
+            return fp.read()
 
-@pytest.fixture
-def ch1_pre_note() -> str:
-    """Sample parenthesized note #1"""
-    with Path.open(Path(__file__).parent / "samples/8.00/8.00_pre_note.txt", encoding="utf-8") as fp:
-        return fp.read()
+    @pytest.fixture
+    def authors_note(scope="class") -> str:
+        """Sample chapter #3"""
+        with Path.open(Path(__file__).parent / "samples/10.22_R/authors_note.txt", encoding="utf-8") as fp:
+            return fp.read()
 
+    def test_parens_pre_note_and_pre_authors_note(
+        self, html_content: BeautifulSoup, authors_note: str, text_content: str
+    ):
+        """A chapter may have a parenthesized pre-note in addition to an Author's note
+        at the start of the chapter"""
+        data = parse_chapter_content(html_content)
 
-@pytest.fixture
-def ch2_html_content() -> BeautifulSoup:
-    """Sample chapter #2"""
-    with Path.open(Path(__file__).parent / "samples/TheRoots3/chapter.html", encoding="utf-8") as fp:
-        soup = BeautifulSoup(fp)
-        soup.get("html")
-        return soup
+        pre_note = data.get("pre_note")
+        if pre_note is None:
+            msg = "Empty pre note"
+            raise ValueError(msg)
 
+        assert (
+            pre_note
+            == "(A fellow author and friend of mine, Quill, is releasing a new story! Blood Eagle: Norse Progression Fantasy is out now on Royalroad! Consider giving it a read:\n\n\nhttps://www.royalroad.com/fiction/91540/blood-eagle-norse-progression-fantasy)\n"
+        )
 
-@pytest.fixture
-def ch2_text_content() -> str:
-    """Sample Author's Note #2"""
-    with Path.open(Path(__file__).parent / "samples/TheRoots3/chapter.txt", encoding="utf-8") as fp:
-        return fp.read()
+        authors_note = data.get("authors_note")
+        if authors_note is None:
+            msg = "Empty pre note"
+            raise ValueError(msg)
 
+        assert authors_note == authors_note
 
-@pytest.fixture
-def ch2_authors_note() -> str:
-    """Sample Author's Note #2"""
-    with Path.open(Path(__file__).parent / "samples/TheRoots3/authors_note.txt", encoding="utf-8") as fp:
-        return fp.read()
+        chapter_text = data.get("text")
+        if chapter_text is None:
+            raise ValueError
 
+        assert chapter_text == text_content
 
-@pytest.fixture
-def ch2_pre_note() -> str:
-    """Sample pre note #2"""
-    with Path.open(Path(__file__).parent / "samples/TheRoots3/pre_note.txt", encoding="utf-8") as fp:
-        return fp.read()
-
-
-@pytest.fixture
-def ch3_html_content() -> BeautifulSoup:
-    """Sample chapter #3"""
-    with Path.open(Path(__file__).parent / "samples/10.22_R/chapter.html", encoding="utf-8") as fp:
-        soup = BeautifulSoup(fp)
-        soup.get("html")
-        return soup
-
-
-@pytest.fixture
-def ch3_text_content() -> str:
-    """Sample chapter #3"""
-    with Path.open(Path(__file__).parent / "samples/10.22_R/chapter.txt", encoding="utf-8") as fp:
-        return fp.read()
-
-
-@pytest.fixture
-def ch3_authors_note() -> str:
-    """Sample chapter #3"""
-    with Path.open(Path(__file__).parent / "samples/10.22_R/authors_note.txt", encoding="utf-8") as fp:
-        return fp.read()
-
-
-# ------------------------------------------------------------------------
-# ch1 tests
-# ------------------------------------------------------------------------
-def test_text_content(ch1_html_content: BeautifulSoup, ch1_text_content: str):
-    """Most author's notes appear at the end of the chapter with the typical 'Author's Note' indicator"""
-    data = parse_chapter_content(ch1_html_content)
-    assert data.get("text") == ch1_text_content
-
-
-def test_author_note_at_start():
-    """Some author's notes appear at the start of the chapter with the typical 'Author's Note' indicator"""
-
-
-def test_author_note_at_end(ch1_html_content: BeautifulSoup, ch1_authors_note: str):
-    """Most author's notes appear at the end of the chapter with the typical 'Author's Note' indicator"""
-    data = parse_chapter_content(ch1_html_content)
-    assert data.get("authors_note") == ch1_authors_note
-
-
-def test_parenthesized_author_note_at_start(ch1_html_content: BeautifulSoup, ch1_pre_note: str):
-    """Sometimes pirateaba provides a short aside in parentheses in the first couple lines"""
-    data = parse_chapter_content(ch1_html_content)
-    assert data.get("pre_note") == ch1_pre_note
-
-
-def test_ignore_fanart_attributions():
-    """Many chapters include fanart appended to the end of the chapter which should not be inluded in the chapter text"""
-    # TODO
-
-
-# ------------------------------------------------------------------------
-# ch2 tests
-# ------------------------------------------------------------------------
-def test_text_does_not_contain_authors_note(ch2_html_content: BeautifulSoup):
-    """The chapter text should not contain any text from author's note"""
-    data = parse_chapter_content(ch2_html_content)
-    authors_note = data.get("authors_note")
-    if authors_note is None:
-        msg = "Empty author's note"
-        raise ValueError(msg)
-    for line in authors_note.split("\n"):
-        if not line.isspace and line != "":
-            assert line not in data.get("text")
-
-
-def test_signed_pre_note_detected(ch2_html_content: BeautifulSoup):
-    """Signed pre-notes by the author should not be detected by parser"""
-    data = parse_chapter_content(ch2_html_content)
-    pre_note = data.get("pre_note")
-
-    if pre_note is None:
-        msg = "Empty author's note"
-        raise ValueError(msg)
-
-    assert (
-        pre_note
-        == """Did you click ‘Slam’ on last chapter and find the other two parts? If you didn’t, go and do that.
-—pirateaba
-"""
-    )
-
-
-def test_signed_pre_note_not_in_chapter_text(ch2_html_content: BeautifulSoup):
-    """Signed pre-notes by the author should not be included in the chapter text"""
-    data = parse_chapter_content(ch2_html_content)
-    pre_note = data.get("pre_note")
-
-    if pre_note is None:
-        msg = "Empty author's note"
-        raise ValueError(msg)
-    for line in pre_note.split("\n"):
-        if not line.isspace and line != "":
-            assert line not in data.get("text")
-
-
-# ------------------------------------------------------------------------
-# ch3 tests
-# ------------------------------------------------------------------------
-def test_parens_pre_note_and_pre_authors_note(
-    ch3_html_content: BeautifulSoup, ch3_authors_note: str, ch3_text_content: str
-):
-    """A chapter may have a parenthesized pre-note in addition to an Author's note
-    at the start of the chapter"""
-    data = parse_chapter_content(ch3_html_content)
-
-    pre_note = data.get("pre_note")
-    if pre_note is None:
-        msg = "Empty pre note"
-        raise ValueError(msg)
-
-    assert (
-        pre_note
-        == """(A fellow author and friend of mine, Quill, is releasing a new story! Blood Eagle: Norse Progression Fantasy is out now on Royalroad! Consider giving it a read:
-
-
-https://www.royalroad.com/fiction/91540/blood-eagle-norse-progression-fantasy)
-"""
-    )
-
-    authors_note = data.get("authors_note")
-    if authors_note is None:
-        msg = "Empty pre note"
-        raise ValueError(msg)
-
-    assert authors_note == ch3_authors_note
-
-    chapter_text = data.get("text")
-    if chapter_text is None:
-        raise ValueError
-
-    assert chapter_text == ch3_text_content
+    @pytest.mark.skip
+    def test_ignore_fanart_attributions(self, html_content: BeautifulSoup):
+        """Many chapters include fanart appended to the end of the chapter which should not be included in the chapter text"""
+        text = parse_chapter_content(html_content).get("text")
+        assert text is not None
+        assert "Ko-Fi:" not in text
+        assert "Instagram:" not in text
+        assert "Twitter:" not in text
 
 
 # TODO: chapter may have marked Author's Note at start and end of chapter
+
+# TODO: confirm digest/hash consistency
