8ef60b3c4793702c9341154c099bc42be995c129
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-09-23 13:33:40 -0400

    build: optimize RefTypeChapter update command

--- stats/management/commands/update_reftype_chapters.py
+++ stats/management/commands/update_reftype_chapters.py
@@ -1,36 +1,34 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.db.models import F, Subquery
-from stats.models import Chapter, TextRef, RefType, RefTypeChapter
+from stats.models import Chapter, RefType, RefTypeChapter
 
 
 class Command(BaseCommand):
     """Database build command"""
 
-    help = "Update database from chapter source HTML and other metadata files"
+    help = "Update ReftypeChapter DB view"
 
     def handle(self, *_args, **_options) -> None:  # noqa: ANN002, ANN003
+        new_reftypechapters = []
         for rt in RefType.objects.all():
-            textref_query = (
-                TextRef.objects.filter(type=rt)
-                .annotate(chapter_id=F("chapter_line__chapter__id"))
-                .order_by("chapter_id")
-                .distinct("chapter_id")
-                .values("chapter_id")
-                .all()
-            )
             try:
-                for c in Chapter.objects.filter(id__in=Subquery(textref_query)):
-                    (
-                        ref_type_chapter,
-                        ref_type_chapter_created,
-                    ) = RefTypeChapter.objects.get_or_create(type=rt, chapter=c)
-                    if ref_type_chapter_created:
-                        self.stdout.write(self.style.SUCCESS(f"Created RefTypeChapter for {rt.name} - {c.title}"))
-                    else:
-                        self.stdout.write(
-                            self.style.WARNING(f"RefTypeChapter for {rt.name} - {c.title} already exists."),
-                        )
+                chapters = Chapter.objects.raw(
+                    """
+                    SELECT DISTINCT(cl.chapter_id) as id
+                    FROM stats_textref AS tr
+                        JOIN stats_chapterline AS cl ON tr.chapter_line_id = cl.id
+                        JOIN stats_reftype AS rt ON rt.id = tr.type_id
+                    WHERE rt.name=%s
+                """,
+                    [rt.name],
+                )
+
+                for chapter in chapters:
+                    new_reftypechapters.append(RefTypeChapter(type=rt, chapter=chapter))
+
+                self.stdout.write(self.style.WARNING(f"Creating RefTypeChapters for {rt.name}..."))
 
             except KeyboardInterrupt as exc:
                 msg = "Update paused. Keyboard interrupt received."
                 raise CommandError(msg) from exc
+
+        RefTypeChapter.objects.bulk_create(new_reftypechapters, ignore_conflicts=True)
