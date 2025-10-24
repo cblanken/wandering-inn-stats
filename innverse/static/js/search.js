0d1752b0951b476131bd62df877e813411db361d
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2025-10-24 11:07:40 -0400

    fix: fix broken icon replacement
    After removing the top-level feather dependency <link>
    in base.html the search page and gallery component need
    to use the static feather spritesheet to replace icons
    instead of the `feather.replace()` function.

--- innverse/static/js/search.js
+++ innverse/static/js/search.js
@@ -2,12 +2,10 @@
 let search_btn = document.querySelector("#search_bar_btn");
 
 function replace_search_icon(feather_name) {
-  let icon = document.createElement("i");
-  icon.setAttribute("data-feather", feather_name);
-  icon.classList.add("animate-spin-slow", "absolute", "left-2", "pointer-events-none");
-
-  search_btn.replaceChild(icon, search_btn.firstElementChild);
-  feather.replace();
+  let svg = search_btn.querySelector("svg.feather");
+  let icon = svg.querySelector("use");
+  icon.setAttribute("href", `/static/icons/feather-sprite.svg#${feather_name}`);
+  svg.classList.add("animate-spin-slow", "absolute", "left-2", "pointer-events-none");
 }
 
 search_btn.addEventListener("click", e => {
@@ -19,11 +17,9 @@ let header = document.querySelector("#textref-search-form");
 
 function replace_header_fold_icon(feather_name) {
   let header_fold_btn = document.querySelector("#header-fold-btn")
-  let icon = document.createElement("i")
-  icon.classList.add("inline");
-  icon.setAttribute("data-feather", feather_name)
-  header_fold_btn.replaceChild(icon, header_fold_btn.firstElementChild);
-  feather.replace()
+  let svg = header_fold_btn.querySelector("svg.feather");
+  let icon = svg.querySelector("use");
+  icon.setAttribute("href", `/static/icons/feather-sprite.svg#${feather_name}`);
 }
 
 function fold_header(btn) {
@@ -43,20 +39,24 @@ function unfold_header(btn) {
 }
 
 // Fold per setting in localStorage
-let header_fold_btn = document.querySelector("#header-fold-btn")
-let is_header_folded = localStorage.getItem("header_folded");
-if (is_header_folded === "true") {
-  fold_header(header_fold_btn)
-}
-
-// Add fold click event
-header_fold_btn.addEventListener("click", (e) => {
-  if (localStorage.getItem("header_folded") === "true") {
-    unfold_header(e.target)
-  } else {
-    fold_header(e.target)
+try {
+  let header_fold_btn = document.querySelector("#header-fold-btn")
+  let is_header_folded = localStorage.getItem("header_folded");
+  if (is_header_folded === "true") {
+    fold_header(header_fold_btn)
   }
-  window.dispatchEvent(new Event('resize'));
-})
+
+  // Add fold click event
+  header_fold_btn.addEventListener("click", (e) => {
+    if (localStorage.getItem("header_folded") === "true") {
+      unfold_header(e.target)
+    } else {
+      fold_header(e.target)
+    }
+    window.dispatchEvent(new Event('resize'));
+  })
+} catch (error) {
+  // Unable to update fold icon, so do nothing
+}
 
 let all_content_input = document.querySelector("#id_all_content");
